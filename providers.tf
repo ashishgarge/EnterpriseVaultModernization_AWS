b690df1eafb65b5fe104477786a80a2e81f8ce74
ashish garge <ashish.garge@yahoo.com>
2026-07-22 23:29:22 +0530

    added the terraform backend to providers

--- providers.tf
+++ providers.tf
@@ -4,6 +4,12 @@ terraform {
       version = "= 5.33.0"
     }
   }
+  backend "s3" {
+    bucket = "ev-modernization-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
